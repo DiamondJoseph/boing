bf315891590e86f07343ff90ac65a6cab4e546a7
Joseph Ware <29740989-jbw46@users.noreply.gitlab.com>
2026-08-06 19:09:30 +0100

    feat: minimal application

---
+++ src/test/java/com/example/boing/BoingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.boing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
